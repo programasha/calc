8b76339171efc41680a3dbb6376f545c0bc204a1
Masha Ushakova <masha.a.ushakova@icloud.com>
2019-02-27 23:17:16 +0300

    version 1.1

--- calc.py
+++ calc.py
@@ -1,15 +1,26 @@
 while True:
     s= input()
-    s.split(' ')
-    if s[1]== '+':
-        print(int(s[0]) + int(s[2]))
-    elif s[1]== '-':
-        print(int(s[0])- int(s[2]))
-    elif s[1]== '*':
-        print(int(s[0])*int(s[2]))
-    elif s[1]== '/':
-        print(int(s[0])/int(s[2]))
-    elif s[1]=='^':
-        print(int(s[0])**int(s[2]))
+    if s== "exit":
+        break
+    num1, op, num2 = s.split(' ')
+    num1= int(num1)
+    num2= int(num2)
+    if op== '+':
+        print(num1 + num2)
+    elif op== '-':
+        print(num1 - num2)
+    elif op== '*':
+        print(num1 * num2)
+    elif op== '/':
+        if num2 ==0:
+            print("DO NOT DIVIDE BY ZERO!!!")
+        else:
+            print(num1 / num2)
+    elif op=='^':
+        print(num1 ** num2)
+    elif op=='%':
+        print(num1 % num2)
+    elif op== 'r':
+        print(num1 ** (1/num2))
 
     
